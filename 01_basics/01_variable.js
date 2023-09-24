74a2cb0d625fce019e584e2639893c4e5a77540d
shaqib <104290413+Shaqib27@users.noreply.github.com>
2023-09-24 14:24:32 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=12345;
+let accountEmail="shaqib246@gmail.com";
+var accountpassword="54321";
+accountcity="patna";
+let accountstate;
+
+// not use the var because it is a golbal variable type any one can change the value
+
+
+
+// accountId=2
+accountEmail="hsah@gmail.com"
+accountpassword="1234"
+accountcity="kolkata"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+//javascript remove the extra spaces
+
+
+//Data type 
+//nn ss bb u
+/*  number
+    null
+    symbol
+    string=""
+    boolean=true/false
+    bigint(for very lager value)
+    undefined 
+    */
+
+console.log(typeof "shaqib")//string
+console.log(typeof(null))//object
+console.log(typeof undefined);//undefined
+
