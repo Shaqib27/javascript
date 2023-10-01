aae2003d9c74bd0c3ccb695b7366908a3400eb40
shaqib <104290413+Shaqib27@users.noreply.github.com>
2023-10-01 18:06:23 +0000

    done(1/10/23)

--- 03_basic/IIFE.js
+++ 03_basic/IIFE.js
@@ -1,1 +1,17 @@
-//Immediately Invoked Function Expression
+//Immediately Invoked Function Expression(IIFE)
+
+//their are two main reasion why we 
+//1.to excute as the program start  (immdiately excute)
+//2.we don't want to polute our variable with the golbal variable 
+
+//kai bar global scope ke polution se problem hotii hai of is se bachne ke liye hm log (IIFE) ka use karten hain
+
+(function chai()
+{
+    console.log("hello user")
+})();  //here use ; to stop the excution or end the funtion
+
+
+((name)=>{
+    console.log(`DB connected is ${name}`)
+})("shaqib")
